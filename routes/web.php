cb562b69cf365aa433b9d979c3d7bccfba291f84
rivbmbh <rivaldoangelobomboah@gmail.com>
2024-12-04 13:21:15 +0800

    instal laravel

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+
+    // $user->token
+});
